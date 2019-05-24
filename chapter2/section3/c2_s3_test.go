d1046f3c884b533285789ffcf15b5ba82ff25121
pcmid <plzcmid@gmail.com>
2019-05-25 04:26:22 +0800

    Chapter II, Section 3

--- chapter2/section3/c2_s3_test.go
+++ chapter2/section3/c2_s3_test.go
@@ -1,1 +1,27 @@
 package section3
+
+import "testing"
+
+func Test_Merge(t *testing.T) {
+	nums := []int{1, 3, 5, 2, 4, 6, 7}
+
+	Merge(nums)
+
+	for i := range nums {
+		if nums[i] != i+1 {
+			t.Error("failure")
+		}
+	}
+}
+
+func Test_MergeSort(t *testing.T) {
+	nums := []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+
+	MergeSort(nums)
+
+	for i := range nums {
+		if nums[i] != i+1 {
+			t.Error("failure")
+		}
+	}
+}
--- chapter2/section3/merge_sort.go
+++ chapter2/section3/merge_sort.go
@@ -1,1 +1,54 @@
 package section3
+
+func MergeSort(nums []int) {
+
+	numsL := nums[:len(nums)/2]
+	numsR := nums[len(nums)/2:]
+
+	if len(numsL) > 1 {
+		MergeSort(numsL)
+	}
+	if len(numsR) > 1 {
+		MergeSort(numsR)
+	}
+	Merge(nums)
+
+}
+
+func Merge(nums []int) {
+	var numLS []int
+	var numRS []int
+	for i := range nums[:len(nums)/2] {
+		numLS = append(numLS, nums[i])
+
+	}
+	for i := range nums[len(nums)/2:] {
+		numRS = append(numRS, nums[len(nums)/2+i])
+
+	}
+
+	{
+		l := 0
+		r := 0
+
+		for i := 0; i < len(nums); i++ {
+			if l >= len(numLS) {
+				nums[i] = numRS[r]
+				r++
+				continue
+			}
+			if r >= len(numRS) {
+				nums[i] = numLS[l]
+				l++
+				continue
+			}
+			if numLS[l] <= numRS[r] {
+				nums[i] = numLS[r]
+				l++
+			} else {
+				nums[i] = numRS[r]
+				r++
+			}
+		}
+	}
+}
